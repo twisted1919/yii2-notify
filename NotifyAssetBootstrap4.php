355e1371f017602fea91d8fb1b17fc350b51c4dd
twisted1919 <totyoserbangeorgecristian@yahoo.com>
2019-11-18 11:30:31 +0200

    added support for bs4

---
+++ NotifyAssetBootstrap4.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace twisted1919\notify;
+
+class NotifyAssetBootstrap4 extends NotifyAsset
+{
+    /**
+     * @var array
+     */
+    public $depends = [
+        'yii\bootstrap4\BootstrapAsset',
+    ];
+}
